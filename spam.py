e390e1ae82181b0ca57ea494d6ae46141aba3b9c
volodymyr <82139002+hduwiz@users.noreply.github.com>
2022-04-06 01:06:22 +0300

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,22 @@
+from telethon.sync import TelegramClient, events
+
+##with love from hduwiz  , TG : godlxss <3
+print()
+hashtg = input("hash : ")
+idtg = int(input("id : "))
+px = int(input("sms : "))
+per = input("id chela: ")
+mes = input("text : ")
+
+api_id = idtg 
+api_hash = hashtg
+
+with TelegramClient('proxy', api_id, api_hash) as client:
+	for i in range(px):
+		client.send_message(per, mes)
+		client.send_message(per, mes)
+		client.send_message(per, mes)
+		client.send_message(per, mes)
+		client.send_message(per, mes)
+		client.send_message(per, mes)
+		client.send_message(per, mes)
