6938e9f02434afb5f747c96074839e43fb6ff8bf
volodymyr <82139002+hduwiz@users.noreply.github.com>
2022-04-06 06:03:20 +0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,14 +3,17 @@ from telethon.sync import TelegramClient, events
 ##with love from hduwiz  , TG : godlxss <3
 print("\033[1;32m with love from hduwiz<3  \n")
 print("\033[1;0m\n")
+
 hashtg = input("hash : ")
 idtg = int(input("id : "))
 px = int(input("how much : "))
-per = input("id chela: ")
+per = input("username : ")
 mes = input("text : ")
 
 api_id = idtg 
 api_hash = hashtg
+
+print("Thank You! , Glory of Ukraine! UA")
 
 with TelegramClient('proxy', api_id, api_hash) as client:
 	for i in range(px):
